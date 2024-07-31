5d68cdf660b40facb58c46ed807df3fb2a4447bd
Juani <jibyrab@gmail.com>
2024-07-31 11:26:54 -0300

    First commit

---
+++ Cap1/1.01/program.c
@@ -0,0 +1,8 @@
+/*Ejecute el programa "hola, mundo." en sus sitema.*/
+
+#include <stdio.h> /*Biblioteca standar*/
+
+int main(){
+    printf("hello world.\n");
+    return 0;
+}
---
+++ Cap1/1.02/program.c
@@ -0,0 +1,9 @@
+/*Experimente el comportamiento de printf() cuando la cadena contiene '\c'*/
+
+#include <stdio.h>
+
+int main(){
+    printf("\c");
+}
+
+/* output: warning: unknown escape sequence: '\c'*/
